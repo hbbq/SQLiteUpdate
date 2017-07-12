05c3c097bbbcd36bb318b89d21db259b89795ffa
Henrik Bergqvist <henrik.bergqvist@parttrap.com>
2017-07-12 21:52:52 +0200

    Remove unused packages

--- src/TestApp/Program.cs
+++ src/TestApp/Program.cs
@@ -19,30 +19,18 @@ namespace TestApp
 
             SQLiteConnection.CreateFile(filename);
 
-            //var scripts = new List<SQLiteUpdate.UpdateScript>
-            //{
-            //    new SQLiteUpdate.UpdateScript
-            //    {
-            //        Identity = "00001",
-            //        Script = "create table a (a int, b text)",
-            //    },
-            //    new SQLiteUpdate.UpdateScript
-            //    {
-            //        Identity = "00002",
-            //        Script = "insert into a (a, b) values (1, 'a')",
-            //    }
-            //};
-
             using(var cn = new SQLiteConnection($@"Data source={filename}"))
             {
+
                 SQLiteUpdate.Db.UpdateFromResources(cn, System.Reflection.Assembly.GetExecutingAssembly(), "TestApp.DbScripts");
-            //    SQLiteUpdate.Db.UpdateFromScripts(cn, scripts);
+
                 using (var da = new SQLiteDataAdapter("select * from test", cn))                
                 using (var ds = new DataSet())
                 {
                     da.Fill(ds);
                     Console.WriteLine(ds.GetXml());
                 }
+
             }
 
             Console.ReadLine();
